19cf8a5bce75444d51e6709a58ae7bb8c178edb1
Tihh Gonçalves <tihh.gon@gmail.com>
2017-11-24 23:13:19 -0200

    atualizando textos

--- js/popup.js
+++ js/popup.js
@@ -5,19 +5,17 @@ $(document).ready(function(){
         var texto;
         texto  = 'Olá {{NOME}}, tudo bem?';
         texto += '||';
-        texto += 'Me interesso em fazer uma proposta pra você. Mas antes, gostaria de tirar algumas dúvidas:';
-        texto += '||';
-        texto += '[PERGUNTAS]';
+        texto += 'Me interesso em fazer uma proposta pra você. Mas antes, gostaria de conhecer mais seu projeto.';
         texto += '||';
         texto += 'Desde já agradeço sua atenção e resposta.';
         texto += '||';
-        texto += 'Abraços.';
+        texto += 'Abraços,';
         texto += '|';
         texto += 'TIHH GONÇALVES';
         texto += '|';
         texto += 'tiago@tiago.art.br';
         texto += '|';
-        texto += '(47) 9 9650-6687 (Whats App e Telegram)';
+        texto += '(47) 9 9650-6687 (WhatsApp e Telegram)';
 
         //copyTextToClipboard(texto);
         btn_status('Escrito!', '#btn_pergunta');
@@ -31,19 +29,21 @@ $(document).ready(function(){
     $('#btn_proposta').click(function(){
 
         var texto;
-        texto = 'Bacana seu projeto {{NOME}}. Será um prazer trabalhar pra você.';
+        texto = 'Bacana o seu projeto {{NOME}}. Será um prazer trabalhar pra você.';
         texto += '||';
         texto += 'Aproveito pra convidar você a conhecer um pouco do meu trabalho: www.tiago.art.br.';
         texto += '||';
         texto += 'Fico no aguardo de uma resposta sua.';
         texto += '||';
-        texto += 'Abraços.';
+        texto += 'Observação: Propostas acima de R$1.000, somente [por fora do 99freelas] com 50% de entrada (cobrado no boleto bonitinho) e 50% depois de tudo pronto e aprovado.';
+        texto += '||';	
+        texto += 'Abraços,';
         texto += '|';
         texto += 'TIHH GONÇALVES';
         texto += '|';
         texto += 'tiago@tiago.art.br';
         texto += '|';
-        texto += '(47) 9 9650-6687 (Whats App e Telegram)';
+        texto += '(47) 9 9650-6687 (WhatsApp e Telegram)';
 
         //copyTextToClipboard(texto);
         btn_status('Escrito!', '#btn_proposta');
